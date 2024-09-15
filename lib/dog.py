c6c6a6e99c6ae13158cf343973858491ea086e8c
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-09-15 18:35:23 +0300

    finished  the init-self

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    # pass
+    def __init__(self,name,breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Dog")
+# return("Dog")
+
+
+# class Dog1:
+#     def breed(self,name):
+#         return(f"breed{name}")
+    
+    
+    
+# bosko = Dog1
+
+# bosko.breed = "German"
+# print(bosko.breed)
